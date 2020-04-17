51586991eeb9103d43cfb0d1853005ef63ae03dc
BanallanX <62731680+BanallanX@users.noreply.github.com>
2020-04-17 19:56:54 -0400

    Add files via upload

--- BenN_Python_2.7.16(withoutparse).py
+++ BenN_Python_2.7.16(withoutparse).py
@@ -4,6 +4,6 @@ import os.path
 
 print("Beginning download of Python 2.7.4 - Release Date: April 6, 2013")
 
-urllib.urlretrieve('https://www.python.org/ftp/python/2.7.4/python-2.7.4.amd64.msi', 'Python2_7_4.msi')
+urllib.urlretrieve('https://www.python.org/ftp/python/2.7.4/python-2.7.4.amd64.msi', 'ben-python2_7_4.msi')
 
 print("Completed download")
